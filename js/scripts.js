76804669fe50291dc0ce203dec1b6d927f7139e0
Sally Devitry <sallydevitry@yahoo.com>
2020-10-17 20:27:20 -0600

    good site now

--- js/scripts.js
+++ js/scripts.js
@@ -40,3 +40,19 @@
         target: "#sideNav",
     });
 })(jQuery); // End of use strict
+
+$(document).ready(function() {
+    // executes when HTML-Document is loaded and DOM is ready
+   console.log("document is ready");
+     
+   
+     $( ".card" ).hover(
+     function() {
+       $(this).addClass('shadow').css('cursor', 'pointer'); 
+     }, function() {
+       $(this).removeClass('shadow');
+     }
+   );
+     
+   // document ready  
+   });
